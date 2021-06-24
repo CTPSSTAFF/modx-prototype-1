86dd07cb3ec4178da488177849a2583b69b21be3
Ben Krepp <bkrepp@ctps.org>
2021-06-24 16:32:43 -0400

    Enabled matplotlib pan/zoom/etc.

--- taz_maps/taz_total_nonmotorized_trips_comparison.py
+++ taz_maps/taz_total_nonmotorized_trips_comparison.py
@@ -18,6 +18,12 @@ import hvplot.xarray
 import cartopy.crs as ccrs
 
 
+# In[ ]:
+
+
+get_ipython().run_line_magic('matplotlib', 'notebook')
+
+
 # In[2]:
 
 
@@ -225,6 +231,7 @@ joined_df = taz_gdf.join(delta_total_nm_trips_df.set_index('omxid'), on='omxid')
 # Make a static map of total auto trips by origin TAZ
 joined_df.plot("delta_total_nm", figsize=(10.0,8.0), cmap='plasma', legend=True)
 plt.title('Change in Total Non-motorized Trips by Origin TAZ')
+plt.show()
 
 
 # In[22]:
