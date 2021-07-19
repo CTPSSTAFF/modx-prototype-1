6eb070ceb5aa61a04a8db60e408b8c2d8758ce33
Ben Krepp <bkrepp@ctps.org>
2021-07-19 14:03:09 -0400

    Create bbox_of_gdf.py

---
+++ utilities/bbox_of_gdf.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import geopandas as gp
+
+# Return the bounding box of all the features in a geo-dataframe.
+# The bounding box is returned as a list of the form [minx, miny, maxx, maxy].
+#
+def bbox_of_gdf(gdf):
+    bounds_tuples = gdf['geometry'].map(lambda x: x.bounds)
+    bounds_dicts = []
+    for t in bounds_tuples:
+        temp = { 'minx' : t[0], 'miny' : t[1], 'maxx' : t[2], 'maxy' : t[3] }
+        bounds_dicts.append(temp)
+    # end_for
+    bounds_df = pd.DataFrame(bounds_dicts)
+    minx = bounds_df['minx'].min()
+    miny = bounds_df['miny'].min()
+    maxx = bounds_df['maxx'].max()
+    maxy = bounds_df['maxy'].max()
+    retval = [ minx, miny, maxx, maxy]
+    return retval
+# end_def bbox_of_gdf()
