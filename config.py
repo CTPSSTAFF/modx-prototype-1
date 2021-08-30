2e8a813158a5cf3cc90e53b90798426f82b1e5f8
Ben Krepp <bkrepp@ctps.org>
2021-08-30 09:37:52 -0400

    File for users to specify locations of 2-3 input and 1 output directory.

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+#
+# *** USER ACTION REQUIRED: ***
+# Specify the values of the four variables below, and SAVE YOUR CHANGES before running any MoDX notebook.
+# If you are not referencing a 'comparison scenario', set the value of comparison_scenario_dir to None, e.g.,
+#     comparison_scenario_dir = None
+#
+base_scenario_dir = 'G:/Regional_Modeling/1A_Archives/LRTP_2018/2016 Scen 00_08March2019_MoDXoutputs/'
+comparison_scenario_dir = 'G:/Regional_Modeling/1A_Archives/LRTP_2018/2040 NB Scen 01_MoDXoutputs/'
+reference_data_dir = 'G:/Data_Resources/modx/'
+sandbox_dir = 'S:/my_modx_output_dir/'
